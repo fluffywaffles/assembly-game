a29c39f7f232f18f125299bdcf5d6a93b53df93b
skorlir <skorlir@gmail.com>
2016-03-07 17:56:33 -0600

    Better Animation and abstract Shader a bit

--- game.asm
+++ game.asm
@@ -53,28 +53,27 @@ PLAYER_VX DWORD 0
 PLAYER_VY DWORD 0
 
 ;; Animations
-NukeAnimation Animation { 0, 3, SIZEOF EECS205BITMAP,, OFFSET nuke_000 }
+NukeAnimation Animation { 0, 3, SIZEOF EECS205BITMAP, }
 
 ;; Shaders
-FadeInOut Shader { 0, 2, 2, 0ffh, ?, ?, ?, ? }
+opacities BYTE 4 DUP(0ffh), 4 DUP(0dah), 4 DUP(09h), 4 DUP(0)
+FadeInOut Shader { 0, 2, 2, 0ffh, LENGTHOF opacities, OFFSET opacities, ?, ?, ?, ? }
 
 ;; Fighter (Player)
 Fighter AnimatedCharacter { 0, 100, 100,,, }
 FighterCollider EECS205RECT { ?, ?, ?, ? }
-FighterAnimation Animation { 0, 3, SIZEOF EECS205BITMAP,, OFFSET fighter_000 }
+FighterAnimation Animation { 0, 3, SIZEOF EECS205BITMAP, OFFSET fighter_000 }
 
 ;; other
-asteroid_rotation FXPT 0
-asteroid_collider EECS205RECT <?, ?, ?, ?>
-space_down BYTE 0
+Asteroid AnimatedCharacter { 0, 319, 239,,, }
+AsteroidCollider EECS205RECT { ?, ?, ?, ? }
+AsteroidAnimation Animation { 0, 1, SIZEOF EECS205BITMAP, OFFSET asteroid_000 }
 
 rot_offset FXPT 0c900h ; approximately pi/2 to maximum possible accuracy
 
 wCollide DWORD l7c, l7o, l7l,
                l7l, l7i, l7d,
                l7e, l7exclamation
-
-opacities BYTE 4 DUP(0ffh), 4 DUP(0dah), 4 DUP(09h), 4 DUP(0)
 
 .CODE
 
@@ -103,27 +102,30 @@ UpdateTime PROC USES eax
   ret
 UpdateTime ENDP
 
-CalculateCollider PROC USES eax edx edi bmpPtr:PTR EECS205BITMAP, colliderPtr:PTR EECS205RECT, x:DWORD, y:DWORD, rotation:FXPT
+CalculateCollider PROC USES eax edx character:PTR AnimatedCharacter
   LOCAL left:DWORD, top:DWORD, right:DWORD, bottom:DWORD
   LOCAL r_sqrt2:FXPT, sina:FXPT, cosa:FXPT, xcomp:FXPT, ycomp:FXPT
   LOCAL x_cos:FXPT, x_sin:FXPT, y_cos:FXPT, y_sin:FXPT, xpcos:FXPT, xpsin:FXPT, ypcos:FXPT, ypsin:FXPT
 
-  invoke UnpackBitmap, bmpPtr
-  mov edi, colliderPtr
+  mov esi, character
 
-  invoke EdgesFromCenter, x, _dwWidth
-  mov [edi], eax     ; left
-  mov [edi + 8], edx ; right
+  ASSUME esi:PTR AnimatedCharacter
+
+  invoke UnpackBitmap, [esi].anim.frame_ptr
+
+  invoke EdgesFromCenter, [esi].pos_x, _dwWidth
+  mov [esi].collider.dwLeft, eax ; left
+  mov [esi].collider.dwRight, edx ; right
   mov left, eax
   mov right, edx
 
-  invoke EdgesFromCenter, y, _dwHeight
-  mov [edi + 4], eax  ; top
-  mov [edi + 12], edx ; bottom
+  invoke EdgesFromCenter, [esi].pos_y, _dwHeight
+  mov [esi].collider.dwTop, eax  ; top
+  mov [esi].collider.dwBottom, edx ; bottom
   mov top, eax
   mov bottom, edx
 
-  mov edx, rotation
+  mov edx, [esi].rotation
   add edx, rot_offset
   neg edx
   invoke FixedSin, edx
@@ -154,7 +156,7 @@ CalculateCollider PROC USES eax edx edi bmpPtr:PTR EECS205BITMAP, colliderPtr:PT
   invoke AXP, r_sqrt2, sina, 0
   mov ycomp, eax
 
-  mov eax, x
+  mov eax, [esi].pos_x
   sub eax, xcomp
   mov x_cos, eax
   add eax, xcomp
@@ -167,7 +169,7 @@ CalculateCollider PROC USES eax edx edi bmpPtr:PTR EECS205BITMAP, colliderPtr:PT
   add eax, ycomp
   mov xpsin, eax
 
-  mov eax, y
+  mov eax, [esi].pos_y
   sub eax, xcomp
   mov y_cos, eax
   add eax, xcomp
@@ -189,16 +191,6 @@ CalculateCollider PROC USES eax edx edi bmpPtr:PTR EECS205BITMAP, colliderPtr:PT
 
   ret
 CalculateCollider ENDP
-
-CalculatePlayerCollider PROC
-  invoke CalculateCollider, Fighter.anim.frame_ptr, OFFSET Fighter.collider, Fighter.pos_x, Fighter.pos_y, Fighter.rotation
-  ret
-CalculatePlayerCollider ENDP
-
-CalculateAsteroidCollider PROC
-  invoke CalculateCollider, OFFSET asteroid_000, OFFSET asteroid_collider, 319, 239, asteroid_rotation
-  ret
-CalculateAsteroidCollider ENDP
 
 UpdatePlayer PROC USES eax
   mov eax, m_x
@@ -249,10 +241,10 @@ DrawPlayer PROC USES eax
 DrawPlayer ENDP
 
 DrawAsteroid PROC USES ebx
-  mov ebx, asteroid_rotation
+  mov ebx, Asteroid.rotation
   add ebx, delta_t
-  mov asteroid_rotation, ebx
-  invoke RotateBlit, OFFSET asteroid_000, 319, 239, ebx, 255, 0
+  mov Asteroid.rotation, ebx
+  invoke RotateBlit, Asteroid.anim.frame_ptr, Asteroid.pos_x, Asteroid.pos_y, Asteroid.rotation, 255, 0
   ret
 DrawAsteroid ENDP
 
@@ -297,6 +289,7 @@ GamePlay PROC USES eax ebx
 
   ENDIF
 
+  mov esi, Fighter.shader.cm_src
   mov ah, Fighter.shader.cm_index
   mov al, Fighter.shader.cm_delta
   mov dh, Fighter.shader.cm_repeat
@@ -310,26 +303,44 @@ GamePlay PROC USES eax ebx
   end_up:
     cmp ah, dl
     jl  set_opacity
-    neg Fighter.shader.cm_delta
+    .if Fighter.shader.cm_repeat == 2
+      ; reverse
+      neg Fighter.shader.cm_delta
+    .elseif Fighter.shader.cm_repeat == 1
+      ; stop
+      mov Fighter.shader.cm_delta, 0
+    .else
+      ; wrap back to repeat position
+      mov ah, Fighter.shader.cm_repeat
+    .endif
     jmp set_opacity
   end_down:
     cmp ah, 0
     jne set_opacity
-    neg Fighter.shader.cm_delta
+    .if Fighter.shader.cm_repeat == 2
+      ; reverse
+      neg Fighter.shader.cm_delta
+    .elseif Fighter.shader.cm_repeat == 1
+      ; stop
+      mov Fighter.shader.cm_delta, 0
+    .else
+      ; wrap back to repeat position
+      mov ah, Fighter.shader.cm_repeat
+    .endif
   set_opacity:
     mov Fighter.shader.cm_index, ah
     movzx ecx, ah
-    movzx ecx, opacities[ecx]
+    movzx ecx, BYTE PTR [esi + ecx]
     mov Fighter.shader.colorMask, cl
 
   invoke UpdatePlayer
   invoke DrawPlayer
-  invoke CalculatePlayerCollider
+  invoke CalculateCollider, OFFSET Fighter
 
   invoke DrawAsteroid
-  invoke CalculateAsteroidCollider
+  invoke CalculateCollider, OFFSET Asteroid
 
-  invoke CheckIntersectRect, OFFSET Fighter.collider, OFFSET asteroid_collider
+  invoke CheckIntersectRect, OFFSET Fighter.collider, OFFSET Asteroid.collider
   cmp eax, 1
   jne no_collision
 
@@ -344,8 +355,14 @@ GamePlay ENDP
 GameInit PROC
   mov Fighter.shader.cm_index, 0
   mov Fighter.shader.cm_delta, 1
-  mov Fighter.shader.cm_repeat, 0
+  mov Fighter.shader.cm_repeat, ShaderRepeatReverse
   mov Fighter.shader.colorMask, 0ffh
+  mov Fighter.shader.cm_src_len, LENGTHOF opacities
+  mov Fighter.shader.cm_src, OFFSET opacities
+
+  mov Fighter.anim.frame_ptr, OFFSET fighter_000
+  mov Asteroid.anim.frame_ptr, OFFSET asteroid_000
+
 	ret
 GameInit ENDP
 
